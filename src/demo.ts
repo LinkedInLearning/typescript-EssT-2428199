3c6f8db20334a4c6ba2de1deee5dacabc8a35f0e
Jess Chadwick <jesschadwick@gmail.com>
2022-01-13 00:03:30 -0500

    02_06e

--- src/demo.ts
+++ src/demo.ts
@@ -3,9 +3,20 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
+interface UserContact<TExternalId> {
+    id: number
+    name: string
+    username: string
+    externalId: TExternalId
+    loadExternalId(): Task<TExternalId>
+}
+
+function clone<T1, T2 extends T1>(source: T1): T2 {
     return Object.apply({}, source);
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
-const b = clone(a)
+const b = clone<Contact, UserContact>(a)
+
+const dateRange = { startDate: Date.now(), endDate: Date.now() }
+const dateRangeCopy = clone(dateRange)
