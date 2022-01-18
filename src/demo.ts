d15b191fe7916f0ffe10e0340ccec392862ab6c2
Jess Chadwick <jesschadwick@gmail.com>
2022-01-18 00:40:33 -0500

    03_01e

--- src/demo.ts
+++ src/demo.ts
@@ -1,39 +1,16 @@
 type ContactName = string;
+type ContactStatus = "active" | "inactive" | "new"
+type ContactBirthDate = Date | number | string
 
-enum ContactStatus {
-    Active = "active",
-    Inactive = "inactive",
-    New = "new"
-}
-
-interface Contact {
+interface Contact  {
     id: number;
     name: ContactName;
-    birthDate?: Date;
+    birthDate?: ContactBirthDate;
     status?: ContactStatus;
-}
-
-interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
-}
-
-function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
 }
 
 let primaryContact: Contact = {
     id: 12345,
-    name: "Jamie Johnson"
+    name: "Jamie Johnson",
+    status: "active"
 }
