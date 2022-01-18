9c606b54a3ead863b260212eb4c96c8897adfdf4
Jess Chadwick <jesschadwick@gmail.com>
2022-01-18 00:54:54 -0500

    03_03b

--- src/demo.ts
+++ src/demo.ts
@@ -2,24 +2,29 @@ type ContactName = string;
 type ContactStatus = "active" | "inactive" | "new"
 type ContactBirthDate = Date | number | string
 
-interface Contact  {
+interface Contact {
     id: number;
     name: ContactName;
     birthDate?: ContactBirthDate;
     status?: ContactStatus;
-    email: string;
 }
 
-let primaryContact: Contact = {
-    id: 12345,
-    name: "Jamie Johnson",
-    status: "active"
+function toContact(nameOrContact) {
+    if (typeof nameOrContact === "string") {
+        return {
+            id: 0,
+            name: nameOrContact,
+            status: "active"
+        }
+    }
+    else if (typeof nameOrContact === "object") {
+        return {
+            id: nameOrContact.id,
+            name: nameOrContact.name,
+            status: nameOrContact.status
+        }
+    }
+    else {
+        return null;
+    }
 }
-
-type ContactFields = keyof Contact
-
-function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName]
-}
-
-const value = getValue({ min: 1, max: 200 }, "")
