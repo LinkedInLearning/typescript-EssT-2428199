c82d393b55cbab7747193cce96c193fdcb222eed
Jess Chadwick <jesschadwick@gmail.com>
2022-01-19 23:00:49 -0500

    05_04e

--- src/demo.ts
+++ src/demo.ts
@@ -30,6 +30,28 @@ function authorize(role: string) {
     }
 }
 
+function freeze(constructor: Function) {
+    Object.freeze(constructor)
+    Object.freeze(constructor.prototype)
+}
+
+function singleton<T extends { new(...args: any[]): {} }>(constructor: T) {
+    return class Singleton extends constructor {
+        static _instance = null;
+
+        constructor(...args) {
+            super(...args);
+            if (Singleton._instance) {
+                throw Error("Duplicate instance")
+            }
+
+            Singleton._instance = this
+        }
+    }
+}
+
+@freeze
+@singleton
 class ContactRepository {
     private contacts: Contact[] = [];
 
