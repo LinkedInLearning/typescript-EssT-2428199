c3ede8f6050d59c82239d7efebc9a7ef4e6c47a8
Jess Chadwick <jesschadwick@gmail.com>
2022-01-19 22:10:41 -0500

    04_02e

--- src/demo.ts
+++ src/demo.ts
@@ -14,18 +14,22 @@ interface Contact {
     email: string;
 }
 
-interface Query {
+interface Query<TProp> {
     sort?: 'asc' | 'desc';
-    matches(val): boolean;
+    matches(val: TProp): boolean;
 }
 
-type ContactQuery = Partial<Record<keyof Contact, Query>>;
+//type ContactQuery = Partial<Record<keyof Contact, Query>>;
+
+type ContactQuery = {
+    [TProp in keyof Contact]?: Query<Contact[TProp]>
+}
 
 function searchContacts(contacts: Contact[], query: ContactQuery) {
     return contacts.filter(contact => {
         for (const property of Object.keys(contact) as (keyof Contact)[]) {
             // get the query object for this property
-            const propertyQuery = query[property];
+            const propertyQuery = query[property] as Query<Contact[keyof Contact]>;
             // check to see if it matches
             if (propertyQuery && propertyQuery.matches(contact[property])) {
                 return true;
