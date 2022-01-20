d5da8ea6c4a28ac321ad2605c809c38fd42bc260
Jess Chadwick <jesschadwick@gmail.com>
2022-01-19 22:44:25 -0500

    05_02e

--- src/demo.ts
+++ src/demo.ts
@@ -13,6 +13,23 @@ const currentUser = {
     }
 }
 
+function authorize(target: any, property: string, descriptor: PropertyDescriptor) {
+    const wrapped = descriptor.value
+
+    descriptor.value = function () {
+        if (!currentUser.isAuthenticated()) {
+            throw Error("User is not authenticated");
+        }
+
+        try {
+            return wrapped.apply(this, arguments);
+        } catch (ex) {
+            // TODO: some fancy logging logic here
+            throw ex;
+        }
+    }
+}
+
 class ContactRepository {
     private contacts: Contact[] = [];
 
