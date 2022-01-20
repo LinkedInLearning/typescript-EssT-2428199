0236951fac38ce35911f55c91b91ec16274654f9
Jess Chadwick <jesschadwick@gmail.com>
2022-01-19 22:49:23 -0500

    05_03e

--- src/demo.ts
+++ src/demo.ts
@@ -13,15 +13,20 @@ const currentUser = {
     }
 }
 
-function authorize(target: any, property: string, descriptor: PropertyDescriptor) {
-    const wrapped = descriptor.value
+function authorize(role: string) {
+    return function authorizeDecorator(target: any, property: string, descriptor: PropertyDescriptor) {
+        const wrapped = descriptor.value
 
-    descriptor.value = function () {
-        if (!currentUser.isAuthenticated()) {
-            throw Error("User is not authenticated");
+        descriptor.value = function () {
+            if (!currentUser.isAuthenticated()) {
+                throw Error("User is not authenticated");
+            }
+            if (!currentUser.isInRole(role)) {
+                throw Error(`User not in role ${role}`);
+            }
+
+            return wrapped.apply(this, arguments);
         }
-
-        return wrapped.apply(this, arguments);
     }
 }
 
@@ -30,10 +35,6 @@ class ContactRepository {
 
     @authorize("ContactViewer")
     getContactById(id: number): Contact | null {
-        if (!currentUser.isInRole("ContactViewer")) {
-            throw Error("User not authorized to execute this action");
-        }
-
         const contact = this.contacts.find(x => x.id === id);
         return contact;
     }
