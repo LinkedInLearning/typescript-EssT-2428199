9a54269222ab2809d170ae2ccfbd0640d9c555ad
Jess Chadwick <jesschadwick@gmail.com>
2022-02-09 19:45:56 -0500

    04_Challenge

---
+++ app.ts
@@ -0,0 +1,31 @@
+function query<T>(
+    items: T[],
+    query: any // <--- replace this!
+) {
+    return items.filter(item => {
+        // iterate through each of the item's properties
+        for (const property of Object.keys(item)) {
+
+            // get the query for this property name
+            const propertyQuery = query[property]
+
+            // see if this property value matches the query
+            if (propertyQuery && propertyQuery(item[property])) {
+                return true
+            }
+        }
+
+        // nothing matched so return false
+        return false
+    })
+}
+
+const matches = query(
+    [
+        { name: "Ted", age: 12 },
+        { name: "Angie", age: 31 }
+    ],
+    {
+        name: name => name === "Angie",
+        age: age => age > 30
+    })
